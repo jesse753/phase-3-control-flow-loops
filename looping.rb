ec592c79a1e38344ba81dca872eb7c4fa7c0abb5
jesse <jesse.kibaara@student.moringaschool.com>
2023-03-12 22:10:57 +0300

    countdown code passed

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  countdown = 10
+  while countdown > 0
+    puts countdown
+    countdown -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
